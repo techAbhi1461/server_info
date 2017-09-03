700308cf3f337dafa38f8cb89d45bfb33e079e6b
techAbhi1461 <magicalbhakuni@gmail.com>
2017-09-03 18:32:36 -0500

    Added bootstrap and displayed server info

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 
-
+<?php include "server_info.php" ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>System Info</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
     <body>
-    
+        <div class="container">
+        <h1>Server and File Info</h1>
+        <?php if($server): ?>
+            <ul class="list-group">
+            <?php foreach($server as $key => $value): ?>
+                <li class="list-group-item">
+                    <strong><?php echo $key;?></strong>
+                    <?php echo $value;?>
+                </li>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </body>
 </html>
