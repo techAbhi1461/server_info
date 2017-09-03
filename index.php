31ece4cbc9a7a86d05995aad83b2e05f9f608657
techAbhi1461 <magicalbhakuni@gmail.com>
2017-09-03 18:36:14 -0500

    Added display for Client Info in a bootstrap container

--- index.php
+++ index.php
@@ -20,5 +20,18 @@
                 </li>
             <?php endforeach; ?>
         <?php endif; ?>
+
+        <h1>Client Info</h1>
+        <?php if($client): ?>
+            <ul class="list-group">
+            <?php foreach($client as $key => $value): ?>
+                <li class="list-group-item">
+                    <strong><?php echo $key;?></strong>
+                    <?php echo $value;?>
+                </li>
+            <?php endforeach; ?>
+        <?php endif; ?>
+            
+        </div>
     </body>
 </html>
